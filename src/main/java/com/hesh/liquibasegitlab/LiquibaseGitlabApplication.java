085a60d49f2bf5490ca01a78e3d1fee3cb109009
hgamage <hgamage@wiley.com>
2023-10-11 16:25:12 +0530

    feat: initial commit

---
+++ src/main/java/com/hesh/liquibasegitlab/LiquibaseGitlabApplication.java
@@ -0,0 +1,13 @@
+package com.hesh.liquibasegitlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseGitlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseGitlabApplication.class, args);
+	}
+
+}
